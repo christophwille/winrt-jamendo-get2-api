2b273e284f84f3b0ee883f8fbfe7e1db4b4e0dec
Christoph Wille <christophw@alphasierrapapa.com>
2012-10-07 18:04:27 +0200

    About: display version information from manifest

--- Source/Station366/About.xaml.cs
+++ Source/Station366/About.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -21,6 +22,8 @@ namespace Station366
         public About()
         {
             this.InitializeComponent();
+
+            VersionInformationTextBlock.Text = "Version " + GetAppVersion();
         }
 
         private void Support_OnClick(object sender, RoutedEventArgs e)
@@ -31,6 +34,17 @@ namespace Station366
         private void PrivacyStatement_OnClick(object sender, RoutedEventArgs e)
         {
             Windows.System.Launcher.LaunchUriAsync(new Uri(Constants.PrivacyPolicyUrl));
+        }
+
+        // http://www.michielpost.nl/PostDetail_67.aspx
+        private string GetAppVersion()
+        {
+
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+
+            return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
         }
     }
 }
